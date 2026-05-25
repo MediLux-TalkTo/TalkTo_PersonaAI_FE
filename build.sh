597a5190a40266d4ae04ad237a9ef7dc047d52e7
blackcat417 <ssoo174@ewha.ac.kr>
2026-05-25 15:38:37 +0900

    fix: vercel flutter build

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$HOME/flutter"
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 ./flutter
+export PATH="$PATH:$(pwd)/flutter/bin"
 
-export PATH="$HOME/flutter/bin:$PATH"
-
-flutter --version
+flutter doctor -v
 flutter config --enable-web
 flutter pub get
 flutter build web --release
